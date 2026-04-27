e84dbd1e3e9ab83969786d18e3f6c1f9ae665672
s0zvezd1e <matoxa1969.3.1.6@gmail.com>
2026-04-27 03:57:12 +0300

    Задание 5: умножение матриц с CUDA streams
    Результаты:
    - Serial: 25.44 мс, 84.41 GFLOPS
    - Concurrent: 18.33 мс, 117.17 GFLOPS
    - Speedup: 1.39x
    - Корректность: max diff = 0
    
    Реализованы:
    - gemm_serial - последовательная версия
    - gemm_concurrent - конкурентная версия с CUDA streams

--- task5/main.f90
+++ task5/main.f90
@@ -4,10 +4,10 @@ program main
     use gpu_streams_module
     implicit none
 
-    integer, parameter :: M        = 2048
-    integer, parameter :: N        = 2048
-    integer, parameter :: K_large  = 65536   ! 32k столбцов
-    integer, parameter :: NSTREAMS = 8
+    integer, parameter :: M = 512
+    integer, parameter :: N = 512
+    integer, parameter :: K = 4096
+    integer, parameter :: NSTREAMS = 4
 
     real(8), allocatable :: h_A(:,:), h_B(:,:)
     real(8), allocatable :: h_C_serial(:,:), h_C_concurrent(:,:)
@@ -17,21 +17,12 @@ program main
     integer :: istat
 
     write(*,'(a)') '============================================='
-    write(*,'(a,i0,a,i0,a,i0,a,i0)') 'A: ', M,' x ',N,',  B: ',N,' x ',K_large
+    write(*,'(a,i0,a,i0,a,i0,a,i0)') 'A: ', M,' x ',N,',  B: ',N,' x ',K
     write(*,'(a,i0)')   '  streams: ', NSTREAMS
     write(*,'(a)') '============================================='
 
-    allocate(h_A(M, N), h_B(N, K_large), stat=istat)
-    if (istat /= 0) then
-        write(*,'(a)') 'Memory allocation error in main (h_A, h_B)'
-        stop 1
-    end if
-
-    allocate(h_C_serial(M, K_large), h_C_concurrent(M, K_large), stat=istat)
-    if (istat /= 0) then
-        write(*,'(a)') 'Memory allocation error in main (h_C_serial, h_C_concurrent)'
-        stop 1
-    end if
+    allocate(h_A(M, N), h_B(N, K), h_C_serial(M, K), h_C_concurrent(M, K), stat=istat)
+    if (istat /= 0) stop 'Memory allocation error'
 
     call rnd_fill(h_A)
     call rnd_fill(h_B)
@@ -44,22 +35,19 @@ program main
     call gemm_concurrent(h_A, h_B, h_C_concurrent, NSTREAMS, t_concurrent_ms)
     write(*,'(a,f8.2,a)') 'done  ', t_concurrent_ms, ' ms'
 
-    flops = 2.d0 * dble(M) * dble(N) * dble(K_large)
+    flops = 2.d0 * dble(M) * dble(N) * dble(K)
 
     write(*,'(a)') ''
-    write(*,'(a)') '──────────────────────────────────────────────'
-    write(*,'(a25,2x,a8,2x,a8)') 'Mode', 'ms', 'GFLOPS'
-    write(*,'(a)') '──────────────────────────────────────────────'
-    write(*,'(a25,2x,f8.2,2x,f8.3)') 'Serial', &
-        real(t_serial_ms), flops / (dble(t_serial_ms)*1d-3) / 1d9
-    write(*,'(a25,2x,f8.2,2x,f8.3)') 'Concurrent', &
-        real(t_concurrent_ms), flops / (dble(t_concurrent_ms)*1d-3) / 1d9
-    write(*,'(a)') '──────────────────────────────────────────────'
-    write(*,'(a,f5.2)') 'Speedup concurrent/serial: ', &
-        real(t_serial_ms) / real(t_concurrent_ms)
+    write(*,'(a)') '--------------------------------------------------'
+    write(*,'(a25,2x,a8,2x,a10)') 'Mode', 'ms', 'GFLOPS'
+    write(*,'(a)') '--------------------------------------------------'
+    write(*,'(a25,2x,f8.2,2x,f8.3)') 'Serial', t_serial_ms, flops / (dble(t_serial_ms)*1d-3) / 1d9
+    write(*,'(a25,2x,f8.2,2x,f8.3)') 'Concurrent', t_concurrent_ms, flops / (dble(t_concurrent_ms)*1d-3) / 1d9
+    write(*,'(a)') '--------------------------------------------------'
+    write(*,'(a,f5.2)') 'Speedup: ', t_serial_ms / t_concurrent_ms
 
     diff = max_abs_diff(h_C_serial, h_C_concurrent)
-    write(*,'(a,es10.3)') 'Max |serial - concurrent|: ', diff
+    write(*,'(a,es10.3)') 'Max diff: ', diff
 
     deallocate(h_A, h_B, h_C_serial, h_C_concurrent)
 end program main
